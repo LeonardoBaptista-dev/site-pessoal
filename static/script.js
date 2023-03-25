a62a5096b68e00879c7936c85c8b5515777529fe
LeonardoBaptista-dev <leonardobaptista.dev@gmail.com>
2023-03-25 16:05:44 -0300

    seção sobre e habilidades

--- static/script.js
+++ static/script.js
@@ -9,3 +9,16 @@ menuMobile.addEventListener("click", () => {
     : menuMobile.classList.replace("bi-x", "bi-list")
   body.classList.toggle("menu-nav-active")
 });
+
+/* Fecha o menu quando clica em algum intem e muda o icone para list*/
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item=> {
+  item.addEventListener("click", () => {  
+    if (body.classList.contains("menu-nav-active")) {
+      body.classList.remove("menu-nav-active")
+      menuMobile.classList.replace("bi-x", "bi-list")
+    }
+  })
+})
